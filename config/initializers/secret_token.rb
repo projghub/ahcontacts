c3de4859568ab21918cfcb37c48078f5cd6733dd
Billy Bob <billy@hotmail.com>
2013-07-29 13:15:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ahcontacts::Application.config.secret_token = '80ccde6c87409f92ed7ded1a38bc4fda4a04aed7cddd219c482be77dc13d467e4faf5a2f1493ff4c601ed50022cdd75b621583da3b743eb5f2d8003f072cd73f'
